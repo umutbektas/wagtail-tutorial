409c41ae025df4b681030272f5184c290d7206d5
Umut <umut.bektas07@gmail.com>
2019-05-08 22:53:47 +0300

    Flex Page add

---
+++ flex/models.py
@@ -0,0 +1,32 @@
+"""Flexible page."""
+
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+from streams import blocks
+
+class FlexPage(Page):
+    """Flexible page class."""
+
+    template = "flex/flex_page.html"
+
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+    
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+        StreamFieldPanel("content"),
+    ]
+
+
+    class Meta: # noqa
+        verbose_name = "Flex Page"
+        verbose_name_plural = "Flex Pages"
